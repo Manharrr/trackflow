0374b2231dcdd84761d3d285e028d600f11e380a
ManharCK <manhargurukkal2@gmail.com>
2026-09-11 11:35:53 +0530

    last aws routr problem

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,26 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
   withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const host = window.location.hostname;
-  config.baseURL = `http://${host}:8000/api`;
   return config;
 });
 
 export default axiosInstance;
+
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   withCredentials: true,
+// });
+
+// axiosInstance.interceptors.request.use((config) => {
+//   const host = window.location.hostname;
+//   config.baseURL = `http://${host}:8000/api`;
+//   return config;
+// });
+
+// export default axiosInstance;
